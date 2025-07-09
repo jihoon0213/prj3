2ad7d9c23428b0ef11b40701056a7bb9cdd394a2
jihoon0213 <jihun8138@gmail.com>
2025-07-09 17:42:31 +0900

    2025-07-09 게시물 삭제

--- frontend/src/common/AppNavBar.jsx
+++ frontend/src/common/AppNavBar.jsx
@@ -6,7 +6,9 @@ export function AppNavBar() {
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand>PRJ3</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            PRJ3
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
