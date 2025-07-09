290444018a25ba8256043faf8960cfdea19f0ffd
jihoon0213 <jihun8138@gmail.com>
2025-07-09 14:35:18 +0900

    2025-07-09 prj3

---
+++ backend/src/main/java/com/example/backend/board/controller/BoardController.java
@@ -0,0 +1,55 @@
+package com.example.backend.board.controller;
+
+import com.example.backend.board.dto.BoardDto;
+import com.example.backend.board.dto.BoardListInfo;
+import com.example.backend.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+//@Controller
+//@ResponseBody
+@RestController // Controller + ResponseBody
+@RequestMapping("/api/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @GetMapping("{id}")
+    public BoardDto getBoardById(@PathVariable Integer id) {
+        return boardService.getBoardById(id);
+    }
+
+    @GetMapping("list")
+    public List<BoardListInfo> getAllBoards() {
+
+        return boardService.list();
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<Object> add(@RequestBody BoardDto dto) {
+        // 값들이 유효한지 확인
+        boolean result = boardService.validate(dto);
+
+        if (result) {
+            // service에게 넘겨서 일 시키기
+            boardService.add(dto);
+
+            return ResponseEntity.ok().body(Map.of(
+                    "message", Map.of(
+                            "type", "success",
+                            "text", "새 글이 저장되었습니다.")));
+
+        } else {
+            return ResponseEntity.badRequest().body(Map.of(
+                    "message", Map.of(
+                            "type", "error",
+                            "text", "입력한 내용이 유효하지 않습니다.")));
+        }
+    }
+}
---
+++ backend/src/main/java/com/example/backend/board/service/BoardService.java
@@ -0,0 +1,65 @@
+package com.example.backend.board.service;
+
+import com.example.backend.board.dto.BoardListInfo;
+import com.example.backend.board.entity.Board;
+import com.example.backend.board.dto.BoardDto;
+import com.example.backend.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public void add(BoardDto dto) {
+        // entity에 dto의 값 들 옮겨 담고
+        Board board = new Board();
+        board.setTitle(dto.getTitle());
+        board.setContent(dto.getContent());
+        board.setAuthor(dto.getAuthor());
+
+        // repository에 save 실행
+        boardRepository.save(board);
+
+    }
+
+    public boolean validate(BoardDto dto) {
+        if (dto.getTitle() == null || dto.getTitle().trim().isBlank()) {
+            return false;
+        }
+
+        if (dto.getContent() == null || dto.getContent().trim().isBlank()) {
+            return false;
+        }
+
+        if (dto.getAuthor() == null || dto.getAuthor().trim().isBlank()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public List<BoardListInfo> list() {
+        return boardRepository.findAllByOrderByIdDesc();
+    }
+
+    public BoardDto getBoardById(Integer id) {
+        Board board = boardRepository.findById(id).get();
+        BoardDto boardDto = new BoardDto();
+        boardDto.setId(board.getId());
+        boardDto.setTitle(board.getTitle());
+        boardDto.setContent(board.getContent());
+        boardDto.setAuthor(board.getAuthor());
+        boardDto.setInsertedAt(board.getInsertedAt());
+
+        return boardDto;
+
+    }
+}
