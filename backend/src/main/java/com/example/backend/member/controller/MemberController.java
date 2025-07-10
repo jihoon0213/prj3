57613d7637506e8221fad208259b93cc1886e9cc
jihoon0213 <jihun8138@gmail.com>
2025-07-10 10:52:15 +0900

    2025-07-10 회원 가입 진행중

---
+++ backend/src/main/java/com/example/backend/member/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.example.backend.member.controller;
+
+import com.example.backend.member.dto.MemberForm;
+import com.example.backend.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("add")
+    public ResponseEntity<?> add(@RequestBody MemberForm memberForm) {
+//        System.out.println("memberForm = " + memberForm);
+        try {
+            memberService.add(memberForm);
+        } catch (Exception e) {
+            e.printStackTrace();
+            String message = e.getMessage();
+            return ResponseEntity.badRequest().body(
+                    Map.of("message",
+                            Map.of("type", "error",
+                                    "text", message)));
+        }
+
+        return ResponseEntity.ok().body(
+                Map.of("message",
+                        Map.of("type", "success",
+                                "text", "회원 가입 되었습니다."))
+        );
+    }
+
+}
---
+++ backend/src/main/java/com/example/backend/member/service/MemberService.java
@@ -0,0 +1,68 @@
+package com.example.backend.member.service;
+
+import com.example.backend.member.dto.MemberForm;
+import com.example.backend.member.entity.Member;
+import com.example.backend.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.regex.Pattern;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public void add(MemberForm memberForm) {
+
+        if (this.validate(memberForm)) {
+            Member member = new Member();
+            member.setEmail(memberForm.getEmail());
+            member.setPassword(memberForm.getPassword());
+            member.setInfo(memberForm.getInfo());
+            member.setNickName(memberForm.getNickName());
+
+            memberRepository.save(member);
+        }
+
+    }
+
+    private boolean validate(MemberForm memberForm) {
+        // 이미 있는 email 인지
+        Optional<Member> db1 = memberRepository.findById(memberForm.getEmail());
+        if (db1.isPresent()) {
+            throw new RuntimeException("이미 가입된 이메일입니다.");
+        }
+
+        // 이미 있는 nickName 인지
+        Optional<Member> db2 = memberRepository.findByNickName(memberForm.getNickName());
+        if (db2.isPresent()) {
+            throw new RuntimeException("이미 사용 중인 별명입니다.");
+        }
+
+        // email 있는 지?
+        if (memberForm.getEmail().trim().isBlank()) {
+            throw new RuntimeException("이메일을 입력해야 합니다.");
+        }
+        // 형식에 맞는 지?
+        String email = memberForm.getEmail();
+        if (!Pattern.matches("[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", email)) {
+            throw new RuntimeException("이메일 형식에 맞지 않습니다.");
+        }
+        // password 있는 지?
+        if (memberForm.getPassword().isBlank()) {
+            throw new RuntimeException("패스워드를 입력해야 합니다.");
+        }
+        // nickName 있는 지?
+        if (memberForm.getNickName().isBlank()) {
+            throw new RuntimeException("별명을 입력해야 합니다.");
+        }
+
+
+        return true;
+    }
+}
