242ffb92e5fae12e0a6b7aa66d6886acbc2f6f23
jihoon0213 <jihun8138@gmail.com>
2025-07-16 16:27:38 +0900

    2025-07-16 prj3 회원 삭제 시 좋아요 먼저 삭제

--- backend/src/main/java/com/example/backend/member/service/MemberService.java
+++ backend/src/main/java/com/example/backend/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.example.backend.member.service;
 import com.example.backend.board.entity.Board;
 import com.example.backend.board.repository.BoardRepository;
 import com.example.backend.comment.repository.CommentRepository;
+import com.example.backend.like.repository.BoardLikeRepository;
 import com.example.backend.member.dto.*;
 import com.example.backend.member.entity.Auth;
 import com.example.backend.member.entity.Member;
@@ -33,6 +34,7 @@ public class MemberService {
     private final AuthRepository authRepository;
     private final BoardRepository boardRepository;
     private final CommentRepository commentRepository;
+    private final BoardLikeRepository boardLikeRepository;
 
     public void add(MemberForm memberForm) {
 
@@ -114,6 +116,7 @@ public class MemberService {
             for (Board board : byAuthor) {
                 commentRepository.deleteByBoard(board);
             }
+            boardLikeRepository.deleteByMember(db);
 
             // 회원이 쓴 게시물 지우기
             boardRepository.deleteByAuthor(db);
